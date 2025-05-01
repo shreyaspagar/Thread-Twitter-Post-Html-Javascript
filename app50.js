511e316338ff9835a8b7ef0f539d80a3afe69a19
Shreyas Pagar <130757999+shreyaspagar@users.noreply.github.com>
2025-05-01 11:40:36 +0530

    Add files via upload

---
+++ app50.js
@@ -0,0 +1,15 @@
+// const post = {
+//     username : "@shreyaspagar",
+//     content : "This is my #firstPost",
+//     likes : 150,
+//     reposts : 5,
+//     tags : ["@sandipfoundation", "@sitrcoffical"]
+// };
+
+const obj = {
+    1 : "a",
+    2 : "b",
+    true : "c",
+    null : "d",
+    undefined : "e"
+};
